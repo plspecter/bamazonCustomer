a61e928374941a0557c77bcf2838beceeccd7d0c
Perri Specter <plspecter@gmail.com>
2018-10-17 19:32:47 -0400

    bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,29 @@
+//call mysql
+var mysql = require("mysql");
 //call npm package 
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localHost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    //Username
+    user: "root",
+
+    //Your password
+    password: "password123",
+    database: "bamazon"
+
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connect.end();
+});
+
 
 inquirer.prompt([
 
